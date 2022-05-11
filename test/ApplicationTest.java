08af6787997cb27e6bfee9e98b6f1a29adeeca37
lidor <lidoravi@post.bgu.ac.il>
2022-05-12 01:30:54 +0300

    addition of tests in ApplicationTest

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -3,7 +3,10 @@ import Domain.Referee;
 import Service.Application;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -30,15 +33,38 @@ class ApplicationTest {
     @Test
     @DisplayName("Email format is wrong")
     void EmailFormatIsWrong(){
-        assertEquals(false, application.login("maxim@gmail","123456"));
-        assertEquals(false, application.login("maximgmail.com","123456"));
+        assertFalse( application.login("maxim@gmail","123456"));
+        assertFalse( application.login("maximgmail.com","123456"));
     }
 
-    @Test
+
+    //??? need to check if this test checking the 2 emails. if works - delete the test above.
+    @Nested
+    class ParameterizedTests {
+        @DisplayName("Email format is wrong")
+        @ParameterizedTest
+        @ValueSource(strings = {"maxim@gmail", "maximgmail.com"})
+        public void EmailFormatIsWrong(String email) {
+            assertEquals(false, application.login(email, "123456"));
+        }
+    }
+
+
+
+
+        @Test
     @DisplayName("Password format is wrong")
     void PassFormatIsWrong(){
         assertEquals(false, application.login("test1@gmail.com","12345"));
     }
+
+
+        @Test
+        @DisplayName("Email and password are NULL")
+        void EmailAndPasswordAreNULL(){
+            assertFalse(application.login("","123456"));
+            assertFalse(application.login("maxim@gmail.com",""));
+        }
 
     @Test
     @DisplayName("User does not exist in DB")
@@ -48,6 +74,28 @@ class ApplicationTest {
 
 
     //referee registration
+
+    @Test
+    @DisplayName("Some Details Are NULL In fillForm")
+    void SomeDetailsAreNULLInFillForm(){
+        Calendar c1=Calendar.getInstance();
+        c1.set(Calendar.MONTH, 1);
+        c1.set(Calendar.DATE, 1);
+        c1.set(Calendar.YEAR, 2030);
+        // creating a date object with specified time.
+        Date date = c1.getTime();
+        //date is null
+        assertEquals(false, application.fillForm("arnon","123456","test1@gmail.com",
+                "Canada", "0524565643",null,"Main" ));
+        //country is null
+        assertEquals(false, application.fillForm("arnon","123456","test1@gmail.com",
+                "", "0524565643",date,"Main" ));
+        //full name  is null
+        assertEquals(false, application.fillForm("","123456","test1@gmail.com",
+                "Israel", "0524565643",date,"Main" ));
+
+    }
+
     @Test
     @DisplayName("Mail exist in referee registration")
     void MailExist(){
@@ -57,17 +105,10 @@ class ApplicationTest {
     }
 
     @Test
-    @DisplayName("Not insert a date of birth")
-    void NotInsertADateOfBirth(){
-        assertEquals(false,application.fillForm("Lidor Avital","111111","lidor@gmail.com",
-                "Sudan","0541111111",null,"main"));
-    }
-
-    @Test
     @DisplayName("Add referee registration")
     void addRefereeRegistration(){
         Date date = new Date();
-        assertEquals(true,application.fillForm("Lidor Avital","111111","lidor@gmail.com",
+        assertTrue(application.fillForm("Lidor Avital","111111","lidor@gmail.com",
                 "Sudan","0541111111",date,"main"));
     }
 
@@ -86,7 +127,7 @@ class ApplicationTest {
         Referee referee = new Referee("Maxim","123456","maxim@gmail.com","Israel","0546666666",date,"Secondary");
 
         //delete referee from DB
-        assertEquals(false,application.gameAssignment("1",referee,"Tedi",date));
+        assertFalse(application.gameAssignment("1",referee,"Tedi",date));
     }
 
     @Test
@@ -108,7 +149,32 @@ class ApplicationTest {
         Referee referee = new Referee("yuval Avital","111111","yuval@gmail.com",
                 "Russia","0541111111",date,"main");
 
-        assertEquals(true,application.gameAssignment("1234",referee,"Tedi",date));
+        assertTrue(application.gameAssignment("1234",referee,"Tedi",date));
+    }
+    @Test
+    @DisplayName("Some Details Are NULL In Game Assignment")
+    void SomeDetailsAreNULLInGameAssignment(){
+        Calendar c1=Calendar.getInstance();
+        c1.set(Calendar.MONTH, 1);
+        c1.set(Calendar.DATE, 1);
+        c1.set(Calendar.YEAR, 2030);
+        // creating a date object with specified time.
+        Date date = c1.getTime();
+
+        Referee referee = new Referee("yuval Avital","111111","yuval@gmail.com",
+                "Russia","0541111111",date,"main");
+
+        //date is null
+        assertEquals(false, application.gameAssignment("1234",referee,"Tedi",null));
+        //referee is null
+        assertEquals(false, application.gameAssignment("1234",null,"Tedi",date));
+        //game id is null
+        assertEquals(false, application.gameAssignment("",referee,"Tedi",date));
+        //stadium is null
+        assertEquals(false, application.gameAssignment("1234",referee,"",date));
+
+
+
     }
 
 
